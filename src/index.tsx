49dcc4137ca2e05dc3bf3796e8b894f469fe4858
Hieu Cao <hieucaochi25598@gmail.com>
2024-05-31 18:38:50 +0700

    init project

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import TestComponent from "./test";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import TestComponent from './test';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <TestComponent />
-    </div>
-  );
+    return (
+        <div>
+            <TestComponent />
+        </div>
+    );
 };
 
 root.render(<App />);
--- src/test.tsx
+++ src/test.tsx
@@ -1,28 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 const TestComponent = () => {
-  const [count, setCount] = useState(1);
-  const [hieu, setHieu] = useState("hieu");
-  const [cao, setCao] = useState("cao");
-  const [cao1, setCao1] = useState("cao");
-  const [cao2, setCao2] = useState("cao");
-  const [cao3, setCao3] = useState("cao");
-  useEffect(() => {
-    if (count) {
-      console.log(count);
-    }
-  }, []);
+    const [count, setCount] = useState(1);
 
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
+    useEffect(() => {
+        if (count) {
+            console.log(count);
+        }
+    }, [count]);
 
-  return (
-    <div>
-      <div>{count}</div>
-      <button onClick={handleIncrement}>Increment</button>
-    </div>
-  );
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <div>
+            <div>{count}</div>
+            <button onClick={handleIncrement}>Increment</button>
+        </div>
+    );
 };
 
 export default TestComponent;
